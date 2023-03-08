07ba72e3ce4967719421311cdc2ac9d49b83266a
Srinath <srinath001me@gmail.com>
2023-03-08 12:22:07 +0530

    binding added

---
+++ app/src/main/java/com/example/putatoewallet/view/wallet/presentation/PutatoeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.putatoewallet.view.wallet.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.putatoewallet.R
+
+class PutatoeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_putatoe)
+    }
+}
